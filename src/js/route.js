aa5b6c4f914179a16ad4ce54f957e7c61a85707d
Mirror198829 <412366756@qq.com>
2017-08-11 10:21:59 +0800

    上传ng代码

---
+++ src/js/route.js
@@ -0,0 +1,17 @@
+app.config(function($urlRouterProvider, $stateProvider) {
+  $urlRouterProvider.otherwise("/404");
+  $stateProvider
+    .state("uiView", {
+      url: "/uiView",
+      templateUrl: "src/template/uiView.html"})
+    .state("ngForm",{
+      url:'/ngFormDemo',
+      templateUrl:"src/template/ngForm.html"})
+    .state("bootstrap",{
+      url:'/bootstrap',
+      templateUrl:'src/template/bootstrap.html'})
+    .state("404",{
+      url:"/404",
+      template:"<h1 style='text-align:center;margin-top:100px'>I am sorry,找不到页面,404了哟</h1>"
+    })
+});
