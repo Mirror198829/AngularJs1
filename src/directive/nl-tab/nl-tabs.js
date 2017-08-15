afee471d72955397fe0253b2371a3d0b05b0a398
Mirror198829 <412366756@qq.com>
2017-08-15 18:03:23 +0800

    增加ui-pagination指令

--- src/directive/nl-tab/nl-tabs.js
+++ src/directive/nl-tab/nl-tabs.js
@@ -3,5 +3,16 @@ app.directive("nlTabs",function(){
     restrict:"AE",
     templateUrl:'src/directive/nl-tab/nl-tabs.html',
     replace:true,
+    scope:{
+    	tabsData:"="
+    },
+    link:function(scope,ele,attrs){
+    	ele.delegate(".nl-tabs_item","click",function(){
+    		var num=0;
+    		$(this).addClass("active").siblings('.nl-tabs_item').removeClass("active");
+    		num=ele.find('.nl-tabs_item').index($(this));
+    		ele.find(".nl-tabs_content").removeClass("active").eq(num).addClass("active");
+    	})   	
+    }
   }
 })
